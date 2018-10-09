57a2ad82d7e287cfb7ca3f4cbdac6e74b3e79c8b
Jason Wong <jsnwong@ucdavis.edu>
2018-10-09 10:21:50 -0700

    increase player speed

--- javascript/bundle.js
+++ javascript/bundle.js
@@ -674,7 +674,7 @@ class Game {
     coinOptions.y = 500;
     this.entities['coin4'] = new Coin(coinOptions);
     this.coins.push(this.entities.coin4);
-    platformOptions2.x = 2100;
+    platformOptions2.x = 2000;
     this.entities['platform7'] = new Platform(platformOptions2);
     
     
@@ -682,7 +682,7 @@ class Game {
     coinOptions.y = 500;
     this.entities['coin5'] = new Coin(coinOptions);
     this.coins.push(this.entities.coin5);
-    platformOptions2.x = 2400;
+    platformOptions2.x = 2200;
     this.entities['platform8'] = new Platform(platformOptions2)
     
     //add some coins
@@ -1072,7 +1072,7 @@ const MOVE_STATES = ['move', 'fixed']
 class Player extends GameEntity {
   constructor(options) {
     super(options);
-    this.moveSpd = 4;
+    this.moveSpd = 5;
     this.state = 'move';
     this.ropeLen = 0;
     this.ropeAngle;
@@ -1088,22 +1088,26 @@ class Player extends GameEntity {
   move(swingMove){
     switch (this.state) {
       case 'move':
+      // console.log(this.hspd, this.vspd)
+
         let testObj = Object.assign({}, this);
         testObj.x += testObj.hspd + 1;
         
         if(!this.game.collisionCheck(testObj)){
-          this.x += this.hspd;
+          this.x = Math.floor(this.x + this.hspd);
+        } else {
+          testObj.x -= testObj.hspd;
         }
 
-        testObj.y += testObj.vspd;
-        testObj.x -= testObj.hspd - 1;
-        
+        testObj.y += testObj.vspd * 1.5;
+
         if (!this.game.collisionCheck(testObj)) {
           this.y += this.vspd;
         }
 
         else { 
           console.log('collision');
+          this.x += -this.hspd;
         }
         
         this.rotateSpd = 0.05;
@@ -1112,40 +1116,73 @@ class Player extends GameEntity {
 
       case 'swing':
         let center = this.targetPoint;
+        
         this.ropeAngle = Math.atan2(this.targetPoint.y - this.y, this.targetPoint.x - this.x) * 180 / Math.PI;
+        if(this.ropeAngle < 0){
+          this.ropeAngle = 360 + this.ropeAngle
+        }
         if(this.y + this.vspd > this.ropeLen){
           while(!this.y > this.ropeLen ){
             this.y+=1;
           }
         }
-        
+        // let radius;
+        // radius = Math.sqrt(Math.pow(center.x - this.x, 2) + Math.pow(center.y - this.y, 2));
         //to the mathman i never could be:
         //https://math.stackexchange.com/questions/103202/calculating-the-position-of-a-point-along-an-arc
         let nextX = (center.x + swingMove + (this.x - center.x + swingMove) * Math.cos(this.rotateSpd) + (center.y - this.y) * Math.sin(this.rotateSpd));
         let nextY = (center.y + swingMove + (this.y - center.y) * Math.cos(this.rotateSpd) + (this.x - center.x) * Math.sin(this.rotateSpd));
+        
+        // let nextX = (center.x + radius) * Math.cos(this.ropeAngle) ;
+        // let nextY = (center.y + radius) * Math.sin(this.ropeAngle) ;
+        // debugger
+        // let nextY = (center.y + swingMove + (this.y - center.y) * Math.cos(this.rotateSpd) + (this.x - center.x) * Math.sin(this.rotateSpd));
 
         //old working-ish
         // let nextX = (center.x + (this.x - center.x) * Math.cos(this.rotateSpd) + (center.y - this.y) * Math.sin(this.rotateSpd));
         // let nextY = (center.y + (this.y - center.y) * Math.cos(this.rotateSpd) + (this.x - center.x) * Math.sin(this.rotateSpd));
 
-        this.hspd = nextX - this.x + swingMove;
+        this.hspd = nextX - this.x;
         this.vspd = nextY - this.y;
-        if(nextY < this.y && this.vspd > -4){
-          this.vspd -= 1;
-        }
+        // console.log(this.hspd, this.vspd)
+
+        // if(nextY < this.y && this.vspd > -4){
+        //   this.vspd -= 1;
+        // }
         let test = Object.assign({}, this, {x: this.x, y: this.y + this.vspd});
+
+
         if(!this.game.collisionCheck(test)){
           this.y += this.vspd;
-          this.x += this.hspd;
         } 
+
+        test = Object.assign({}, this, {x: this.x + this.hspd, y: this.y});
+        if(!this.game.collisionCheck(test)){
+          this.x = Math.floor(this.x + this.hspd);
+        }
 
         else {
           //slide
           this.vspd = 0;
-          this.x += this.hspd;
+          let testStep = Object.assign({}, this);
+          let sign;
+          if(testStep.hspd > 0){
+            sign = 3;
+          } else {
+            sign = -3;
+          }
+
+          testStep.x += testStep.hspd + sign;
+
+
+          if(!this.game.collisionCheck(testStep)){
+            this.x = Math.floor(this.x + this.hspd);
+          }
           //add bounce
-          // this.rotateSpd = this.rotateSpd * -0.5;
         }
+        // this.vspd = 0;
+        this.vspd = this.vspd * 3/10;
+        // console.log(this.vspd)
         
         break;
 
